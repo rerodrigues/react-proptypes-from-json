320ad6b59d5e4a439f9d2417d73243d825b047ad
Renato Rodrigues <renato.rodrigues@mercadolivre.com>
2019-06-28 18:23:16 -0300

    Add babel

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        browsers: 'defaults',
+        node: 'current',
+      },
+      forceAllTransforms: true,
+    }],
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
+  ],
+  ignore: [
+    'node_modules',
+    'src/dist',
+  ],
+};
